f05cf213259fc7b86eb070831afda8eed75463ab
Jonathan <joniweb01@gmail.com>
2021-04-26 00:53:54 -0300

    Simple Ktor support

---
+++ Redin-Ktor/src/main/kotlin/com/github/jonathanxd/redin/ktor/RedinKtor.kt
@@ -0,0 +1,71 @@
+/**
+ *      Redin-Ktor - Dependency injection built on top of Kores
+ *
+ *         The MIT License (MIT)
+ *
+ *      Copyright (c) 2021 JonathanxD <https://github.com/JonathanxD/Redin>
+ *      Copyright (c) contributors
+ *
+ *
+ *      Permission is hereby granted, free of charge, to any person obtaining a copy
+ *      of this software and associated documentation files (the "Software"), to deal
+ *      in the Software without restriction, including without limitation the rights
+ *      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *      copies of the Software, and to permit persons to whom the Software is
+ *      furnished to do so, subject to the following conditions:
+ *
+ *      The above copyright notice and this permission notice shall be included in
+ *      all copies or substantial portions of the Software.
+ *
+ *      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *      THE SOFTWARE.
+ */
+package com.github.jonathanxd.redin.ktor
+
+import com.github.jonathanxd.redin.BindContext
+import com.github.jonathanxd.redin.Injector
+import com.github.jonathanxd.redin.Redin
+import com.github.jonathanxd.redin.SINGLETON
+import io.ktor.application.*
+
+/**
+ * Provides simple DSL for working with Redin and Ktor.
+ *
+ * # Example:
+ *
+ * ```kotlin
+ * embeddedServer(Netty, port = 7777, host = "0.0.0.0") {
+ *     redin {
+ *         binding {
+ *             bind<Logger>().inSingletonScope().toValue(LoggerFactory.getLogger("Example"))
+ *         }
+ *         injector {
+ *             get<HttpConfiguration>()
+ *             get<RoutingConfiguration>()
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ */
+fun Application.redin(redin: Injector = Redin {  }, ctx: Redin.() -> Unit) {
+    redin.bind {
+        bind<Application>() inScope SINGLETON toValue this@redin
+    }
+    ctx(Redin(redin))
+}
+
+class Redin(private val injector: Injector) {
+    fun binder(ctx: BindContext.() -> Unit) {
+        injector.bind(ctx)
+    }
+
+    fun injector(inj: Injector.() -> Unit) {
+        inj(this.injector)
+    }
+}
